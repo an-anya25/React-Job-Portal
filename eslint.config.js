76c880a88f5690a64e2ccaf30c73c7f79f47f9ec
techmuffin <ananyababu25@gmail.com>
2024-08-01 12:28:58 +0530

    add files

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      "import/no-unresolved": "error",
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          js: "never",
+          jsx: "never",
+        },
+      ],
+    },
+  },
+];
